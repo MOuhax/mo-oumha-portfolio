658c6b94ea785df6d40473df15c06ac1ab928388
Momo <medoumha@gmail.com>
2023-02-18 12:30:10 +0100

    update fofor dep 1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import Nopage from './components/Nopage';
-import Portfoliodata from "./components/Portfoliodata";
-import Blogpage from './components/Blogpage';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+// import Portfoliodata from "./components/Portfoliodata";
+// import Blogpage from './components/Blogpage';
+// import Nav from './components/Nav';
+// import Footer from './components/Footer';
 
 
 import {createBrowserRouter,
@@ -18,20 +18,7 @@ const router = createBrowserRouter ([
     element: <App />,
     errorElement: <Nopage/>,
   },
-  // {
-  //   path: "/blog",
-  //   element: <><Nav/><Portfoliodata /><Footer/></>,
-  //   children: [
-  //     {
-  //       path: "post1",
-  //       element: <Blogpage />,
-  //     },
-  //     {
-  //       path: "post2",
-  //       element: <Blogpage />,
-  //     },
-  //   ],
-  //  }
+  
   
 ])
 
